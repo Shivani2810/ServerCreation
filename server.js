e5a07cfe5885e90e482897e1c39c1ef476a49f49
Shivani2810 <108538382+Shivani2810@users.noreply.github.com>
2026-02-27 12:05:43 +0530

    Post

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const app=express();
 app.listen(3000,()=>(
     console.log("Server at 3000")
 ))
+app.get('/', (req, res) => {
+    res.send("Hello");
+});
+
+
+
+app.post('/users', (req, res) => {
+    res.send("User created");
+});
